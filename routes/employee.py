c617a23458b5aa771dd313f244833b7fa2493c95
Torben Abend <torben.abend@gmail.com>
2026-06-03 13:33:42 +0200

    feat: add new endpoint for creating employees

--- routes/employee.py
+++ routes/employee.py
@@ -1,6 +1,7 @@
 from datetime import date
 from fastapi import APIRouter, Depends
 
+from models import Employee
 from dependencies.services import get_employee_service
 
 
@@ -14,6 +15,14 @@ def list_active_employees(
     return service.list_active_employees(expiry_date)
 
 
-@router.get("/employees")
+@router.get("")
 def list_employees(service = Depends(get_employee_service)):
     return service.list_employees()
+
+
+@router.post("")
+def create_employee(
+        new_employee: Employee,
+        service = Depends(get_employee_service)
+):
+    return service.create_employee(new_employee)
