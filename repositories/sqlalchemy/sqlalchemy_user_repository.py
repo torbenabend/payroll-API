9392ebb17159f8484d3469de0e73ff3e9d0868b9
Torben Abend <torben.abend@gmail.com>
2026-06-22 14:20:16 +0200

    fix: change wrong database query

--- repositories/sqlalchemy/sqlalchemy_user_repository.py
+++ repositories/sqlalchemy/sqlalchemy_user_repository.py
@@ -19,9 +19,9 @@ class SqlAlchemyUserRepository(
         return None
 
     def get_user_role(self, user_id: int) -> Role | None:
-        stmt = select(self.model.role).where(UserDB.id == user_id)
+        stmt = select(self.model).where(UserDB.id == user_id)
         result = self.session.execute(stmt).scalars().first()
-        return Role.model_validate(result)
+        return Role.model_validate(result.role)
         #user = self.get_by_id(user_id)
         #if user:
         #    return Role.model_validate(user.role)
